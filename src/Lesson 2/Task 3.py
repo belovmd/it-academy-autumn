06f3fa76c15663acb764af4d0804a104fc2b64f3
Osiron Glaw <osironglaw@gmail.com>
2020-07-17 03:00:39 +0300

    Rename 'input_variable' to 'number'.

--- src/Lesson 2/Task 3.py
+++ src/Lesson 2/Task 3.py
@@ -1,24 +1,24 @@
 divisor = 10
 digits = 1
 
-input_variable = int(input("Введите n: "))
+number = int(input("Введите n: "))
 
 # Sanity checks - inverting the input if it's negative
 # and aborting if the input has less than two digits.
 
-if input_variable < 0:
-    input_variable *= -1
-elif not input_variable // 10:
+if number < 0:
+    number *= -1
+elif not number // 10:
     print("Must have more than one digit!")
     quit()
 
-while input_variable // divisor:
+while number // divisor:
     digits += 1
     divisor *= 10
 
 for i in range(1, int((digits / 2) + 1)):
-    first_digit = input_variable % (10 ** (digits - i + 1)) // (10 ** (digits - i))
-    last_digit = (input_variable % 10 ** i) // 10 ** (i - 1)
+    first_digit = number % (10 ** (digits - i + 1)) // (10 ** (digits - i))
+    last_digit = (number % 10 ** i) // 10 ** (i - 1)
     if first_digit != last_digit:
         print("Not a palindrome!")
         quit()
