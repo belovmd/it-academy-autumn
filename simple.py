42ee67e01d58b1001034d2951189ea36c462afd9
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-07-17 19:26:56 +0300

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,6 +1,6 @@
 var = int(input("число"))
-for number in range (2, var // 2 + 1):
-  if not var % number:
-    break
+for number in range(2, var // 2 + 1):
+    if not var % number:
+        break
 else:
-  print ('простое')
+    print('простое')
