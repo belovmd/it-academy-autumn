512c52fd531f5784845ddd858b956bc497596ad7
TexnoBY <krivosheev2008@gmail.com>
2020-07-18 20:39:42 +0300

    task2 completed

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -15,7 +15,16 @@ def longest_word(str_):
     """
 
     # write your code here
-    return ''  # write return value here
+    import re
+
+    str_ = re.sub(r'[^\w\s]', '', str_)
+    maxlen = 0
+    long_wors = ''
+    for word in str_.split():
+        if len(word) > maxlen:
+            long_wors = word
+            maxlen = len(word)
+    return long_wors  # write return value here
 
 
 if __name__ == '__main__':
