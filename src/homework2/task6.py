ab74305413224f5bb2de7b74dfcf05efb489570b
TexnoBY <krivosheev2008@gmail.com>
2020-07-18 21:26:14 +0300

    task6 completed

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -13,10 +13,24 @@ def palindrom(n):
     """
 
     # write your code here
-    return ''  # write return value here
+    count = 0
+    temp = n
+    while temp:
+        temp //= 10
+        count += 1
+
+    first = n // 10 ** (count - count // 2)
+    last = n % 10 ** (count // 2)
+    for i in range(1, count // 2 + 1):
+        if not first // 10 ** (count // 2 - i) == last % 10:
+            return False
+        first %= 10 ** (count // 2 - i)
+        last //= 10
+    else:
+        return True
 
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    n = 0
+    n = 1232
     print(palindrom(n))
