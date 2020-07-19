3d368651d1c54f6e19da029b477594febf7cc4b1
AlexKrupenkin <arturm.m@bk.ru>
2020-07-19 13:27:20 +0300

    flke8 ok

--- src/polindrom.py
+++ src/polindrom.py
@@ -3,33 +3,28 @@ while 1:
     if var <= 10:
         print('not')
 
-
     if var == 100:
         print('not')
-
 
     if var == 1000:
         print('not')
 
-
     if var > 10:
         if var < 100:
-            for i in range(1,10):
+            for i in range(1, 10):
                 if i == (var//10):
-                    if i == (var%10):
+                    if i == (var % 10):
                         print('polindrom')
                         break
                     else:
-                     print('not')
-
+                        print('not')
 
     if var > 100:
         if var < 1000:
-          for i in range(1,10):
-            if i == (var//100):
-              if i == (var%10):
-                print('polindrom')
-                break
-              else:
-               print('not')
-
+            for i in range(1, 10):
+                if i == (var//100):
+                    if i == (var % 10):
+                        print('polindrom')
+                        break
+                    else:
+                        print('not')
