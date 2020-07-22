2954bc207c06787227d3c20d329646657736cf22
Osiron Glaw <osironglaw@gmail.com>
2020-07-22 15:09:39 +0300

    Added six home tasks.

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -1,22 +1,25 @@
-"""Определите, является ли число палиндромом (читается слева направо и справа
-    налево одинаково).  Число положительное целое, произвольной длины. Задача
-    требует работать только с числами (без конвертации числа в строку или
-    что-нибудь еще)
-"""
-
-
 def palindrom(n):
-    """Поиск числа фибоначчи.
 
-    :param n: Число.
-    :return: Bool. True или False. Является ли число палиндромом.
-    """
+    divisor = 10
+    digits = 1
+    is_palindrome = False
 
-    # write your code here
-    return ''  # write return value here
+    while n // divisor:
+        digits += 1
+        divisor *= 10
+
+    for i in range(1, int((digits / 2) + 1)):
+        first_digit = n % (10 ** (digits - i + 1)) // (10 ** (digits - i))
+        last_digit = (n % 10 ** i) // 10 ** (i - 1)
+        if first_digit != last_digit:
+            is_palindrome = False
+        else:
+            is_palindrome = True
+
+    return is_palindrome  # write return value here
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # здесь можно сделать ввод из консоли и проверить работу функции
-    n = 0
+    n = 2311323
     print(palindrom(n))
