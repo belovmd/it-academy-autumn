801bf48365828869a8e9a3af5f9716ac14427a92
Osiron Glaw <osironglaw@gmail.com>
2020-07-22 15:09:39 +0300

    Added six home tasks.

--- src/homework2/task3.py
+++ src/homework2/task3.py
@@ -1,23 +1,12 @@
-"""Вводится строка. Требуется удалить из нее повторяющиеся символы и все
-    пробелы. Например, если было введено "abc cde def", то должно быть
-    выведено "abcdef".
-
-   Подсказка: оператор in проверяет, входит ли подстрока в строку или нет.
-"""
-
-
 def sub_string(str_):
-    """Конструирование подстроки.
-
-    :param str_: входная строка
-    :return: строка. Получившееся выражение
-    """
-
-    # write your code here
-    return ''  # write return value here
+    clean_string = ""
+    for char in str_:
+        if char not in clean_string and char != " ":
+            clean_string += char
+    return f"{clean_string}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # здесь можно сделать ввод из консоли и проверить работу функции
-    str_ = ''
+    str_ = "asdfasdfadsf           s fd"
     print(sub_string(str_))
--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -1,23 +1,16 @@
-"""Посчитать количество строчных (маленьких) и прописных (больших) букв в
-    введенной строке. Учитывать только английские буквы.
-"""
-
-
 def count_letters(str_):
-    """Подсчет символов.
-
-    :param str_: входная строка
-    :return: кортеж. (low_number, up_number). low_number - количество строчных,
-                                              up_number - количество пописных.
-    """
-
-    # write your code here
     low_number = 0
     up_number = 0
-    return (low_number, up_number)  # write return value here
+    for char in str_:
+        if char.isupper():
+            up_number += 1
+        elif char.islower():
+            low_number += 1
+
+    return (low_number, up_number)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # здесь можно сделать ввод из консоли и проверить работу функции
-    str_ = ''
+    str_ = "9867565443789ASDFS{}DGSDFGSFDG sdf g"
     print(count_letters(str_))
