91cbbde0789f07b7c0964e6330311286eed1e8d0
TexnoBY <krivosheev2008@gmail.com>
2020-07-22 13:32:10 +0300

    added 5 task and checked PEP8

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -1,47 +1,54 @@
-# #1 https://www.codewars.com/kata/53d40c1e2f13e331fc000c26
-# def pow(A, n, I, multiply):
-#     """
-#     Возвращает x в степени n. Предполагает, что I – это единичная матрица, которая
-#     перемножается с mult, а n – положительное целое
-#     """
-#     if n == 0:
-#         return I
-#     elif n == 1:
-#         return A
-#     elif n % 2:
-#         return multiply(pow(A, n - 1, I, multiply), A)
-#     else:
-#         B = pow(A, n // 2, I, multiply)
-#         return multiply(B, B)
-#
-# def matrix_multiply(A, B):
-#     BT = list(zip(*B))
-#     return [[sum(a * b
-#                  for a, b in zip(row_a, col_b))
-#             for col_b in BT]
-#             for row_a in A]
-#
-# def fib(n):
-#     print(abs(n))
-#     F = pow([[1, 1], [1, 0]], abs(n), [[1, 0], [0, 1]], matrix_multiply)
-#     return -F[0][1] if n < 0 and not n % 2 else F[0][1]
-
-# #2 https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
-# def accum(s):
-#     return '-'.join([char.upper() + char.lower() * i for i, char in enumerate(s)])
-
-# # 3 https://www.codewars.com/kata/54da5a58ea159efa38000836
-# def find_it(seq):
-#     for i in set(seq):
-#         if seq.count(i) % 2:
-#             return i
-
-# # 4 https://www.codewars.com/kata/514b92a657cdc65150000006
-# def solution(number):
-#     return sum([i for i in range(number) if not i % 3 or not i % 5])
+# 1 https://www.codewars.com/kata/53d40c1e2f13e331fc000c26
+def pow(matrix_a, n, identity_matrix, multiply):
+    """
+    Возвращает x в степени n. Предполагает, что I – это единичная матрица, которая
+    перемножается с mult, а n – положительное целое
+    """
+    if n == 0:
+        return identity_matrix
+    elif n == 1:
+        return matrix_a
+    elif n % 2:
+        return multiply(pow(matrix_a, n - 1, identity_matrix, multiply), matrix_a)
+    else:
+        matrix_b = pow(matrix_a, n // 2, identity_matrix, multiply)
+        return multiply(matrix_b, matrix_b)
 
 
+def matrix_multiply(matrix_a, matrix_b):
+    transposed_matrix_b = list(zip(*matrix_b))
+    return [[sum(a * b
+                 for a, b in zip(row_a, col_b))
+            for col_b in transposed_matrix_b]
+            for row_a in matrix_a]
 
-if __name__ == '__main__':
 
-    print(solution(10))
+def fib(n):
+    print(abs(n))
+    f = pow([[1, 1], [1, 0]], abs(n), [[1, 0], [0, 1]], matrix_multiply)
+    return -f[0][1] if n < 0 and not n % 2 else f[0][1]
+
+
+# 2 https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+def accum(s):
+    return '-'.join([char.upper() + char.lower() * i for i, char in enumerate(s)])
+
+
+# 3 https://www.codewars.com/kata/54da5a58ea159efa38000836
+def find_it(seq):
+    for i in set(seq):
+        if seq.count(i) % 2:
+            return i
+
+
+# 4 https://www.codewars.com/kata/514b92a657cdc65150000006
+def solution(number):
+    return sum([i for i in range(number) if not i % 3 or not i % 5])
+
+
+# 5 https://www.codewars.com/kata/513e08acc600c94f01000001
+def rgb(r, g, b):
+    r = r if 256 > r >= 0 else 0 if r < 0 else 255
+    g = g if 256 > g >= 0 else 0 if g < 0 else 255
+    b = b if 256 > b >= 0 else 0 if b < 0 else 255
+    return ('{:02X}' * 3). format(r, g, b)
