ac43229d880ad712bb868c07b656b83970718a89
Alina <alina.zaprudskaya@gmail.com>
2020-07-23 16:33:54 +0300

    +code

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -28,5 +28,5 @@ def count_letters(str_):
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    str_ = 'HFKJGghjfkFJKHD'
+    str_ = ''
     print(count_letters(str_))
