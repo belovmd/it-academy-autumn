c5555000441293d5eee51cf810492c683997968e
Alina <alina.zaprudskaya@gmail.com>
2020-07-23 11:27:37 +0300

    +code

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -14,10 +14,19 @@ def count_letters(str_):
     # write your code here
     low_number = 0
     up_number = 0
+
+    string = 'FJrkbhsg tGJD EGVVghpHF'
+    upper_list = 'QWERTYUIOPLKJHGFDSAZXCVBNM'
+    lower_list = 'qwertyuioplkjhgfdsazxcvbnm'
+    for i in range(len(string)):
+        if string[i] in upper_list:
+            up_number += 1
+        if string[i] in lower_list:
+            low_number += 1
     return (low_number, up_number)  # write return value here
 
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    str_ = ''
+    str_ = 'HHHGllltttJJJG'
     print(count_letters(str_))
