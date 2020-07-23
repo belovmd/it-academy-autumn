3deba4aa023d42e35ff53edf35bd45b0fa5037cb
Alina <alina.zaprudskaya@gmail.com>
2020-07-23 16:50:05 +0300

    +code

---
+++ src/homework2/task7.py
@@ -0,0 +1,17 @@
+#вводится число n. далее 4 слова;
+#вывести кол-во непровторяющихся слов 1 строчкой
+#второй строчкой столько раз повторяется каждое слово
+n = int(input())
+words = []
+count = []
+total_count = 0
+for i in range(n):
+    words += input('word?').split()
+while len(words) > 0:
+    count.append(words.count(words[0]))
+    str0 = words[0]
+    for i in range(words.count(words[0])):
+        words.remove(str0)
+    total_count += 1
+print(total_count)
+print(' '.join(str(a) for a in count))
