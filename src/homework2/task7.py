71eb62773b5672963de0b04e2688218ca1ede922
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-07-26 01:07:33 +0300

    Update task7.py

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -16,7 +16,7 @@ while len(words) > 0:
 print(total_count)
 print(' '.join(str(a) for a in count))
 
-""" Выведите таблицу размером d*d, заполненную числами от 1 до d^2d 
+""" Выведите таблицу размером d*d, заполненную числами от 1 до d^2d
  по спирали, выходящей из левого верхнего угла и закрученной по часовой стрелке,"""
 d = int(input())
 nums = [[int(0) for j in range(d)] for i in range(d)]
@@ -25,7 +25,7 @@ a = d
 while a > 0:
     for i in range(r - 1, r):
         for j in range(r - 1, a):
-                nums[i][j] = nums[i][j - 1] + 1
+            nums[i][j] = nums[i][j - 1] + 1
     for j in range(a - 1, a - 2, -1):
         for i in range(r, a):
             nums[i][j] = nums[i - 1][j] + 1
@@ -33,7 +33,7 @@ while a > 0:
         for j in range(a - 2, r - 2, -1):
             nums[i][j] = nums[i][j + 1] + 1
     for j in range(r - 1, r - 2, -1):
-        for i in range(a - 2,r - 1, -1):
+        for i in range(a - 2, r - 1, -1):
             nums[i][j] = nums[i + 1][j] + 1
     a -= 1
     r += 1
