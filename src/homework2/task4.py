b08d0c60b3dce6ba88273caa88f00d7b33a26bd3
AlexKrupenkin <arturm.m@bk.ru>
2020-07-27 21:02:45 +0300

    решено

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -10,14 +10,25 @@ def count_letters(str_):
     :return: кортеж. (low_number, up_number). low_number - количество строчных,
                                               up_number - количество пописных.
     """
-
     # write your code here
     low_number = 0
     up_number = 0
-    return (low_number, up_number)  # write return value here
+    alef = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+    for letters in str_:
+        if letters in alef:
+            if letters.islower():
+                low_number+=1
+                continue
+            if letters.upper():
+                up_number+=1
+
+    print(low_number,up_number)
+
+    #return (low_number, up_number)  # write return value here
 
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    str_ = ''
-    print(count_letters(str_))
+    str_ = 'fvdfdgASDDWWWSDdedf!!!ваавыапкыеы'
+    #print(count_letters(str_))
+    count_letters(str_)
