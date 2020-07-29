88bb7aca2d031ed7382edd46d27151282e4d4ef7
TexnoBY <krivosheev2008@gmail.com>
2020-07-29 12:17:27 +0300

    decreasing lines

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -1,5 +1,5 @@
 # 1 https://www.codewars.com/kata/53d40c1e2f13e331fc000c26
-'''
+"""
 In this kata you will have to calculate fib(n) where:
 
 fib(0) := 0
@@ -13,7 +13,9 @@ Also, it must correctly handle negative numbers as input.
 HINT I: Can you rearrange the equation fib(n + 2) = fib(n + 1) + fib(n)
 to find fib(n) if you already know fib(n + 1) and fib(n + 2)?
 Use this to reason what value fib has to have for negative values.
-'''
+"""
+
+
 def pow(matrix_a, n, identity_matrix, mult):
     if n == 0:
         return identity_matrix
@@ -42,7 +44,8 @@ def fib(n):
 
 # 2 https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
 '''
-This time no story, no theory. The examples below show you how to write function accum:
+This time no story, no theory. The examples below show you how to write
+function accum:
 
 Examples:
 
@@ -50,6 +53,8 @@ accum("abcd") -> "A-Bb-Ccc-Dddd"
 accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 '''
+
+
 def accum(s):
     return '-'.join([c.upper() + c.lower() * i for i, c in enumerate(s)])
 
@@ -60,6 +65,8 @@ Given an array, find the integer that appears an odd number of times.
 
 There will always be only one integer that appears an odd number of times.
 '''
+
+
 def find_it(seq):
     for i in set(seq):
         if seq.count(i) % 2:
@@ -68,19 +75,27 @@ def find_it(seq):
 
 # 4 https://www.codewars.com/kata/514b92a657cdc65150000006
 '''
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10
+that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
 
-Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+Finish the solution so that it returns the sum of all the multiples
+of 3 or 5 below the number passed in.
 '''
+
+
 def solution(number):
     return sum([i for i in range(number) if not i % 3 or not i % 5])
 
 
 # 5 https://www.codewars.com/kata/513e08acc600c94f01000001
 '''
-The rgb function is incomplete. Complete it so that passing in RGB decimal values will result
-in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255.
-Any values that fall out of that range must be rounded to the closest valid value.
+The rgb function is incomplete.
+Complete it so that passing in RGB decimal values will result
+in a hexadecimal representation being returned.
+Valid decimal values for RGB are 0 - 255.
+Any values that fall out of that range must be rounded
+to the closest valid value.
 The following are examples of expected output values:
 
 rgb(255, 255, 255) # returns FFFFFF
@@ -88,6 +103,8 @@ rgb(255, 255, 300) # returns FFFFFF
 rgb(0,0,0) # returns 000000
 rgb(148, 0, 211) # returns 9400D3
 '''
+
+
 def rgb(r, g, b):
     r = r if 256 > r >= 0 else 0 if r < 0 else 255
     g = g if 256 > g >= 0 else 0 if g < 0 else 255
