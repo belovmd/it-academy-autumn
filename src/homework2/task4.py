90223f439aa39640733c640bfb112a42356c6f2b
AlexKrupenkin <68185699+AlexKrupenkin@users.noreply.github.com>
2020-08-02 18:05:57 +0300

    Update task4.py

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -10,14 +10,25 @@ def count_letters(str_):
     :return: кортеж. (low_number, up_number). low_number - количество строчных,
                                               up_number - количество пописных.
     """
-
     # write your code here
     low_number = 0
     up_number = 0
+    alef = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+    for letters in str_:
+        if letters in alef:
+            if letters.islower():
+                low_number+=1
+                continue
+            if letters.upper():
+                up_number+=1
+
+
+
     return (low_number, up_number)  # write return value here
 
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    str_ = ''
+    str_ = input('Введите предложение:')
     print(count_letters(str_))
+
