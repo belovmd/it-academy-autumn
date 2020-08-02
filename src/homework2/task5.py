b6ed8058be807722d6f418f7eef871b45b5ce85b
AlexKrupenkin <68185699+AlexKrupenkin@users.noreply.github.com>
2020-08-02 18:06:20 +0300

    Update task5.py

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -11,10 +11,31 @@ def fibonacci(n):
     """
 
     # write your code here
-    return ''  # write return value here
+
+    if n == 0:
+        i3 = 0
+
+    if n == 1:
+        i3 = 1
+
+
+    else:
+        i1 = 0
+        i2 = 1
+
+        while n - 1  > 0:
+            i3 = i1+i2
+            i1 = i2
+            i2 = i3
+
+            n-=1
+
+    return i3  # write return value here
 
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    n = 0
+
+    n = int(input('Введите n-ое число Фибоначчи:'))
     print(fibonacci(n))
+
