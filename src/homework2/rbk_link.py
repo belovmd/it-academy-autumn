931666e44a5b51a57ea705e942664be0dead4c66
AlexKrupenkin <68185699+AlexKrupenkin@users.noreply.github.com>
2020-08-05 23:18:53 +0300

    Add files via upload
    получаем все ссылки на главной странице rbc.ru

---
+++ src/homework2/rbk_link.py
@@ -0,0 +1,27 @@
+﻿import requests
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+from django.shortcuts import render
+import operator
+import matplotlib.pyplot as plt
+
+
+# получаем список слов по всем ссылкам
+word_list =()
+def i(r):
+    url_1 = 'https://www.rbc.ru/'
+
+    r = requests.get(url_1).text
+    soup = BeautifulSoup(r)
+    for link in soup.find_all("span", {"class": "main__feed__title"}):
+        content = link.text
+        if  content:         # если есть данные ссылки
+            words = content.lower().split()
+            for each_word in words:
+                word_list.append(each_word)
+
+    print(word_list)
+
+
+
+
