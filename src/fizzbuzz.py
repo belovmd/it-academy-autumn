039d11a6c09be17a575c6ef7879cfc6c945061ff
Daniil Kulbackiy <daniil.kulbackiy@gmail.com>
2020-08-19 19:59:47 +0300

    fix: codestyle

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -7,4 +7,9 @@ FizzBuzz
 """
 
 
-[print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or i) for i in range(1, 101)]
+def fizzbuzz():
+    for i in range(1, 101):
+        print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or i)
+
+
+fizzbuzz()
