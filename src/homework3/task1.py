0d779ed97ddae71a799747fa6a14c7407e301781
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-08-21 18:57:41 +0300

    Update task1.py

--- src/homework3/task1.py
+++ src/homework3/task1.py
@@ -14,5 +14,5 @@ for num in range(1, 101):
     else:
         print(num)
 # 2
-print(['Fizz' * (not el % 3) + 'Buzz' * (not el % 5) \
-       or el for el in range(1, 101)])
+print([('Fizz' * (not el % 3) + 'Buzz' * (not el % 5) or el)
+       for el in range(1, 101)])
