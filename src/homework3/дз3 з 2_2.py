5767379ca88d41392213e974623a5a8f00229894
AlexKrupenkin <68185699+AlexKrupenkin@users.noreply.github.com>
2020-08-24 22:43:47 +0300

    Delete дз3 з 2_2.py

--- src/homework3/дз3 з 2_2.py
+++
@@ -1,9 +0,0 @@
-'''Используйте на предыдущий список slice чтобы получить следующий: ['ab', 'ad', 'bc'].'''
-
-
-g = [i[0]+c[0] for i in 'ab'  for c in 'bcd']
-
-print(g)
-
-s = g[slice(0,1)]+g[slice(2,3)]+g[slice(4,5)]
-print(s)
