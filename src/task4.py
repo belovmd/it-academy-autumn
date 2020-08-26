9809749fb452ea4af0685b4648657b94d4966501
Stepan B <tudum@bk.ru>
2020-08-26 15:52:59 +0300

    add task4

---
+++ src/task4.py
@@ -0,0 +1,14 @@
+"""
+Даны два списка чисел. Посчитайте, сколько различных чисел входит только в один
+из этих списков
+"""
+
+
+def numbers_in_only_one_list(lst1, lst2):
+    return len(set(lst1) ^ set(lst2))
+
+
+if __name__ == '__main__':
+    lst1 = [1, 2, 3, 4, 5, 6, 6, 7]
+    lst2 = [1, 3, 4, 5, 8, 9]
+    print(numbers_in_only_one_list(lst1, lst2))
