2b4f660558a83d525bee5139912e79f52e7d8a9f
Daniil Kulbackiy <daniil.kulbackiy@gmail.com>
2020-09-02 21:33:11 +0300

    homework4

---
+++ src/languages.py
@@ -0,0 +1,16 @@
+"""
+Каждый из N школьников некоторой школы знает Mi языков.
+Определите, какие языки знают все школьники и языки,
+которые знает хотя бы один из школьников.
+"""
+
+
+languages = []
+for student in range(int(input())):
+    languages.append(set())
+    for language in range(int(input())):
+        languages[student].add(input())
+print(languages)
+all_, any_ = set.intersection(*languages), set.union(*languages)
+print(len(all_), *all_, sep='\n')
+print(len(any_), *any_, sep='\n')
