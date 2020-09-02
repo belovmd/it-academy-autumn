8bca31db89f057b7e2401d570b13981130e1faee
TexnoBY <krivosheev2008@gmail.com>
2020-09-02 11:25:07 +0300

    Simpler

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -11,26 +11,16 @@ def palindrom(n):
     :param n: Число.
     :return: Bool. True или False. Является ли число палиндромом.
     """
-
-    # write your code here
-    count = 0
-    temp = n
-    while temp:
-        temp //= 10
-        count += 1
-
-    first = n // 10 ** (count - count // 2)
-    last = n % 10 ** (count // 2)
-    for i in range(1, count // 2 + 1):
-        if not first // 10 ** (count // 2 - i) == last % 10:
-            return False
-        first %= 10 ** (count // 2 - i)
-        last //= 10
-    else:
-        return True
+    check = n
+    revers = 0
+    while n:
+        revers *= 10
+        revers += n % 10
+        n //= 10
+    return check == revers
 
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    n = 1232
+    n = 12521
     print(palindrom(n))
