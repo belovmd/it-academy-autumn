018a4dac5501eac084987c31261c7f2a8cc62819
Egor Brel <DwarfCommunist>
2020-09-06 22:42:56 +0300

    Fix flake8 for task 7

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -91,7 +91,7 @@ print(num_primorial(6))
 
 """
 https://leetcode.com/problems/valid-parentheses/
-Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
 determine if the input string is valid.
 
 An input string is valid if:
