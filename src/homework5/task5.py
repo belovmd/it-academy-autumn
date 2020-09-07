9f3fc7b8bb87ca6ad5dfda02e7de6144ce6c065b
TexnoBY <krivosheev2008@gmail.com>
2020-09-07 13:11:49 +0300

    Task5

--- src/homework5/task5.py
+++ src/homework5/task5.py
@@ -1,7 +1,24 @@
+"""
+Написать программу которая находит ближайшую степень двойки
+к введенному числу.
+10(8), 20(16), 1(1), 13(16)
+"""
+
+
+def near_num(num):
+    step_num = 1
+    ans = 0
+    while step_num < num << 1:
+        current = abs(num - step_num)
+        if abs(num - ans) > current:
+            ans = step_num
+        step_num <<= 1
+    return ans
 
 
 def main():
-    pass
+    num = 12
+    print(near_num(num))
 
 
 if __name__ == '__main__':
