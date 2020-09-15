7c3e6a0703526292760c4c25628a1b79d86ce19a
Stepan B <tudum@bk.ru>
2020-09-15 15:11:18 +0300

    fix task4

--- src/task4.py
+++ src/task4.py
@@ -5,10 +5,10 @@
 
 
 def numbers_in_only_one_list(lst1, lst2):
-    return len(set(lst1) ^ set(lst2))
+    return len(set(lst1) & set(lst2))
 
 
 if __name__ == '__main__':
-    lst1 = [1, 2, 3, 4, 5, 6, 6, 7]
+    lst1 = [1, 2, 3, 4, 5, 6, 6, 7, 8]
     lst2 = [1, 3, 4, 5, 8, 9]
     print(numbers_in_only_one_list(lst1, lst2))
