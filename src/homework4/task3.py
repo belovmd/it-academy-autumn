5a7d0c0c8cf7fe2b578517f257e302aba8b2fdb8
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-09-16 16:33:47 +0300

    Update task3.py

--- src/homework4/task3.py
+++ src/homework4/task3.py
@@ -6,7 +6,7 @@
 lst1 = [8, 4, 5, 3, 5, 3, 78, 3, 56, 25, 54, 6, 4, 23]
 lst2 = [8, 4, 5, 3, 5, 3, 38, 74, 52, 75, 24, 234, 456, 24]
 counter = 0
-dct1, dct2 = {el:1 for el in lst1}, {el:1 for el in lst2}
+dct1, dct2 = {el: 1 for el in lst1}, {el: 1 for el in lst2}
 for key in dct1:
     if key in dct2:
         counter += 1
