931fc8f7417a93d72cd1a5101af5f64415d23945
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-09-16 16:37:04 +0300

    Update task6.py

--- src/homework4/task6.py
+++ src/homework4/task6.py
@@ -1,47 +1,17 @@
-# Каждый из N школьников некоторой школы знает Miязыков.
-# Определите, какие языки знают все школьники и языки,
-# которые знает хотя бы один из школьников.
-# Входные данные
-# Первая строка входных данных содержит количество
-# школьников N. Далее идет N чисел Mi, после каждого из
-# чисел идет Mi строк, содержащих названия языков,
-# которые знает i-й школьник. 
-# Пример входных данных:
-#     3          # N количество школьников
-# 2          # M1 количество языков первого школьника
-# Russian    # языки первого школьника
-# English
-# 3          # M2 количество языков второго школьника
-# Russian
-# Belarusian
-# English
-# 3
-# Russian
-# Italian
-# French
+# Во входной строке записан текст. Словом считается 
+# последовательность непробельных символов идущих подряд, 
+# слова разделены одним или большим числом пробелов или 
+# символами конца строки. Определите, сколько различных
+# слов содержится в этом тексте
 
 
-stud_num = int(input('количество учеников?'))
-dct_lngs = {}
-for _ in range(stud_num):
-    lngs_num = int(input('сколько языков знает товарищ?'))
-    for lng in range(lngs_num):
-        lng = input('собственно язык?')
-        dct_lngs[lng] = dct_lngs.get(lng, 0) + 1
-counter_all = 0
-all_lst = []
-counter_any = 0
-any_lst = []
-for key, value in dct_lngs.items():
-    if value == stud_num:
-        counter_all += 1
-        all_lst.append(key)
+text = 'gfijhf,fkdhk v  kgd,kjfg ,lflksf.'
+count = 0
+for el in range(1,len(text)):
+    if el == 1 and text[0].isalpha():
+        count += 1
+    elif text[el].isalpha() and not text[el-1].isalpha():
+        count += 1
     else:
-       counter_any += 1
-       any_lst.append(key)
-print(counter_all)
-for el in all_lst:
-    print(el)
-print(counter_any)
-for el in any_lst:
-    print(el)
+        continue
+print(count)
