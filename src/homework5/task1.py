ee9798a613cbff457eab8915ce24456af096722d
MarchenkoRuslan <n3mo07@bk.ru>
2020-09-21 00:00:30 +0300

    task 1

---
+++ src/homework5/task1.py
@@ -0,0 +1,36 @@
+import inspect
+from src.homework5 import functions
+
+# Оформите решение задач из прошлых домашних работ в функции.
+# Напишите функцию runner. (все станет проще когда мы изучим модули,
+# getattr и setattr)
+# runner() – все фукнции вызываются по очереди
+# runner(‘func_name’) – вызывается только функцию func_name.
+# runner(‘func’, ‘func1’...) - вызывает все переданные функции
+
+
+def runner(*args):
+    if not args:
+        run_list = [el for el in dir(functions) if not el.startswith('__')]
+    else:
+        run_list = [*args]
+
+        for foo in run_list:
+            try:
+                foo = getattr(functions, foo)
+                if inspect.isfunction(foo):
+                    print('Функция: ', foo.__name__)
+                    print('Выполнение: ', foo())
+            except AttributeError:
+                print('Функция не найдена.', foo)
+
+
+runner('euklid', 'text')
+
+# Имена функций:
+# Про страны с городами - 'capitals',
+# Пересечение множеств - 'setTask1',
+# Объединение множеств - 'setTask2',
+# Школьники, которые знают языки - 'languges',
+# Подсчет слов в тексте - 'text',
+# НОД по Евклиду - 'euklid':
