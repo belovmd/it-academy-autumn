b09018b00ae941d46dfb38110470ee57ff8af306
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-09-25 10:43:32 +0300

    Update task7.py

--- src/homework4/task7.py
+++ src/homework4/task7.py
@@ -3,24 +3,6 @@
 # (мы не знаем функции и рекурсию).
 
 
-nums = [int(el) for el in input('enter 2 nums separated by a space').split()]
-mx, mn = max(nums), min(nums)
-while True:
-    div_remain = mx % mn
-    if not div_remain:
-        print(mn)
-        break
-    else:
-        mx, mn = mn, div_remain
-
-
-# ***
-# Позже при изучении вопроса мною был
-# встречен более элегантный алгоритм решения. Вряд ли
-# я в состоянии самостоятельно до него додуматься
-# (хотя кто знает), но я не могу не оставить его здесь.
-
-
 a = int(input())
 b = int(input())
 while a != 0 and b != 0:
