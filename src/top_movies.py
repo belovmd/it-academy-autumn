57a5ca539e3cb4eb869aac8a505851cf41cd4079
Daniil Kulbackiy <daniil.kulbackiy@gmail.com>
2020-10-01 23:07:44 +0300

    refactor

--- src/top_movies.py
+++ src/top_movies.py
@@ -1,3 +1,15 @@
+"""
+В файле хранятся данные с сайта IMDB.
+Скопированные данные хранятся в файле ./data/ ratings.list.
+Откройте и прочитайте файл(если его нет необходимо вывести ошибку).
+Найдите ТОП250 фильмов и извлеките заголовки.
+Программа создает 3 файла
+top250_movies.txt – названия файлов,
+ratings.txt – гистограмма рейтингов,
+years.txt – гистограмма годов.
+"""
+
+
 SEPARATOR = 'New  Distribution  Votes  Rank  Title\n'
 MOVIES_FILE_NAME = 'top250_movies.txt'
 RATINGS_FILE_NAME = 'ratings.txt'
