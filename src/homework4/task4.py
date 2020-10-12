a3567d7166f62ada2083bf2bb1cb4229dd651135
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-10-12 21:01:19 +0300

    Update task4.py

--- src/homework4/task4.py
+++ src/homework4/task4.py
@@ -5,6 +5,6 @@
 lst1 = [8, 4, 5, 3, 5, 3, 78, 3, 56, 25, 54, 6, 4, 23]
 lst2 = [8, 4, 5, 3, 5, 3, 38, 74, 52, 75, 24, 234, 456, 24]
 set1, set2 = set(lst1), set(lst2)
-not_repet_in_lst = len(set1) + len(set2)
+not_repet_in_lsts = len(set1) + len(set2)
 set1.update(set2)
-print(not_repet_in_sets - ((not_repet_in_sets - len(set1)) * 2))
+print(not_repet_in_lsts - ((not_repet_in_lsts - len(set1)) * 2))
