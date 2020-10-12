6661eefeea38847cdd6f8bf0b218a48c95e38a9d
AlinaZaps <66738485+AlinaZaps@users.noreply.github.com>
2020-10-12 15:20:01 +0300

    Update task3.py
    Updated solution with sets

--- src/homework4/task3.py
+++ src/homework4/task3.py
@@ -5,9 +5,8 @@
 
 lst1 = [8, 4, 5, 3, 5, 3, 78, 3, 56, 25, 54, 6, 4, 23]
 lst2 = [8, 4, 5, 3, 5, 3, 38, 74, 52, 75, 24, 234, 456, 24]
-counter = 0
-dct1, dct2 = {el: 1 for el in lst1}, {el: 1 for el in lst2}
-for key in dct1:
-    if key in dct2:
-        counter += 1
-print(counter)
+
+set1, set2 = set(lst1), set(lst2)
+not_repet_in_sets = len(set1) + len(set2)
+set1.update(set2)
+print(not_repet_in_sets - len(set1))
