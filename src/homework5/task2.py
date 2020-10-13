357ffcdd152e197c541f64e879cf1ae828f76ad1
AlinaZaps <alina.zaprudskaya@gmail.com>
2020-10-13 19:42:26 +0300

    task2 creation

---
+++ src/homework5/task2.py
@@ -0,0 +1,39 @@
+# Создайте декоратор, который хранит результаты вызовы
+# функции (за все время вызовов, не только текущий
+# запуск программы)
+
+
+def dec(fun):
+    from datetime import datetime
+    import sqlite3
+
+    conn = sqlite3.connect('dbname.db')  # для проверки мб использована ':memory:'
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS Results (
+       FuncName TEXT,
+       Date TEXT,
+       Result TEXT);
+    """)
+    conn.commit()
+
+    def wrapper():
+        fun_name = fun.__name__
+        date = str(datetime.now)
+        result = fun()
+        # tpl = (fun_name, date, result)
+        cur.execute("""INSERT INTO results
+            VALUES(?, ?, ?);""", (fun_name, date, result))
+        conn.commit()
+        print("successful done")
+
+    return wrapper
+
+
+@dec
+def fun_func():
+    return 275
+
+
+fun_func()
+fun_func()
+fun_func()
