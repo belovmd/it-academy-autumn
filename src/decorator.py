c559f10e95355ac847885049f0ba60849dc8f3e1
Daniil Kulbackiy <daniil.kulbackiy@gmail.com>
2020-10-23 18:43:35 +0300

    refactor decorator

--- src/decorator.py
+++ src/decorator.py
@@ -3,13 +3,15 @@
 результаты вызовы функции
 (за все время вызовов, не только текущий запуск программы)
 """
+from datetime import datetime
 
 
 def save(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        with open('results.txt', 'at') as f:
-            f.write(str(result) + '\n')
+        with open('results.txt', 'a') as f:
+            date_ = datetime.now()
+            f.write(str(date_) + ' ' + str(func.__name__) + ' ' + str(result) + '\n')
         return result
     return wrapper
 
