b3dde383aa2d109fa922beac5e2789684938fb5a
Daniil Kulbackiy <daniil.kulbackiy@gmail.com>
2020-10-23 18:21:34 +0300

    bug-fix in_one_list task

--- src/in_one_list.py
+++ src/in_one_list.py
@@ -8,4 +8,7 @@
 lst_1 = [1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9]
 lst_2 = [2, 3, 4, 10, 11, 11]
 
-print(len(set(lst_1) - set(lst_2)))
+set_1 = set(lst_1)
+set_2 = set(lst_2)
+
+print(len((set_1 - set_2) | (set_2 - set_1)))
