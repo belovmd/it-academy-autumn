9ca678d458594493565aa07104678f97c3d244b5
AlexKrupenkin <68185699+AlexKrupenkin@users.noreply.github.com>
2020-11-06 23:07:49 +0300

    Add files via upload

---
+++ MyBlog/blog/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from  . import models
+from django.contrib.auth.models import  User
+
+
+
+
+class EmailMaterialForm(forms.Form):
+    name = forms.CharField(max_length=20)
+    my_email = forms.EmailField()
+    to_email = forms.EmailField()
+    comment = forms.CharField(required=False,
+                              widget=forms.Textarea)
+
+
+class MaterialForm(forms.ModelForm):
+    class Meta:
+        model = models.Material
+        fields = ('title', 'body', 'material_type')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = models.Comment
+        fields = ('name', 'body')
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Pass',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat pass',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('bad password')
+        return cd['password']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = models.Profile
+        fields = ('birth', 'photo')
